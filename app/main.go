9bd8018182a6a1e047fecd288117d152fbba7f67
mats <20618586+matslarson@users.noreply.github.com>
2023-04-18 15:11:35 -0400

    Basic webapp and Dockerfile

---
+++ app/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.String(200, "Running")
+	})
+
+	api := r.Group("/api")
+
+	api.GET("/demo", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message":   "Automate all the things!",
+			"timestamp": time.Now().Unix(),
+		})
+	})
+
+	r.Run()
+}
