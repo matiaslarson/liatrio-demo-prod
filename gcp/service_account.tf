d6331a7021781fe679ca8441893ff6f2db59d5b6
Matias <60935969+matiaslarson@users.noreply.github.com>
2023-04-20 11:05:00 -0400

    Variablize more things

--- gcp/service_account.tf
+++ gcp/service_account.tf
@@ -1,22 +1,8 @@
 resource "google_service_account" "gh-action-sa" {
   account_id   = "gh-actions"
   display_name = "Github Actions"
-  description = "Service Account for deploying to GKE"
+  description  = "Service Account for deploying to GKE"
 }
-
-
-# resource "google_service_account_iam_binding" "gh-actions-gke-admin" {
-#   service_account_id = google_service_account.gh-action-sa.name
-#   role               = "roles/container.admin"
-#
-#   members = []
-# }
-#
-# resource "google_service_account_iam_binding" "gh-actions-gar" {
-#   service_account_id = google_service_account.gh-action-sa.name
-#   role               = "roles/artifactregistry.writer"
-#   members = []
-# }
 
 resource "google_project_iam_member" "gh-actions-gke" {
   project = var.project_id
